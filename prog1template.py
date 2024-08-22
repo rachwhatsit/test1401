28715e1ed0a8ca5a931bfc2661f540c4fa2fcaba
Rachel Lynne Wilkerson <rachwhatsit@users.noreply.github.com>
2024-08-21 21:00:09 -0500

    Add files via upload

---
+++ prog1template.py
@@ -0,0 +1,9 @@
+#Write a Python 3 program that will print "Hello World!" to the screen.
+#Be sure to use complete documentation.
+#Author:
+#Assignment Title:
+#Assignment Description:
+#Due Date:
+#Date Created:
+#Date Last Modified:
+
